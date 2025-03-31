80c5a652cffd3ad66d939b75f28e4cd26ed9100c
Irene Kilgannon <irene.kilgannon@gmail.com>
2025-03-31 10:03:19 +0100

    improved database tables

--- db.sql
+++ db.sql
@@ -2,31 +2,31 @@ use sewing_patterns;
 
 create table users (
     userID INTEGER AUTO_INCREMENT,
-    name VARCHAR(50),
-    email VARCHAR(320),
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(320) NOT NULL,
     password VARCHAR (255),
-    PRIMARY KEY (personID)
+    PRIMARY KEY (userID)
 );
 
 
 create table patterns (
     patternID VARCHAR(100),
-    brand VARCHAR(100),
-    category  ENUM ('Coat', 'Coordinates', 'Dress', 'Jacket', 'Shirt', 'Skirt', 'Sleepwear', 'Trousers', 'Top'),
-    fabric_type ENUM ('Woven', 'Stretch', 'All'),
+    brand VARCHAR(100) NOT NULL,
+    category  ENUM ('Coat', 'Coordinates', 'Dress', 'Jacket', 'Shirt', 'Skirt', 'Sleepwear', 'Trousers', 'Top') NOT NULL,
+    fabric_type ENUM ('Woven', 'Stretch', 'All') NOT NULL,
     description VARCHAR(255),
-    ownerID INTEGER,
-    PRIMARY KEY patternID,
-    FOREIGN KEY ownerID REFERENCES users (userID)
+    ownerID INTEGER NOT NULL,
+    PRIMARY KEY (patternID),
+    FOREIGN KEY (ownerID) REFERENCES users (userID)
     );
 
 
 create table borrow_logs (
     loanID INTEGER AUTO_INCREMENT,
-    borrow_date DATE,
-    borrowerID INTEGER,
-    patternID VARCHAR(100),
-    PRIMARY KEY loanID,
-    FOREIGN KEY borrowerID REFERENCES users (userID),
-    FOREIGN KEY patternID REFERENCES patterns (patternID)
+    borrow_date DATE NOT NULL,
+    borrowerID INTEGER NOT NULL,
+    patternID VARCHAR(100) NOT NULL,
+    PRIMARY KEY (loanID),
+    FOREIGN KEY (borrowerID) REFERENCES users (userID),
+    FOREIGN KEY (patternID) REFERENCES patterns (patternID)
 );
