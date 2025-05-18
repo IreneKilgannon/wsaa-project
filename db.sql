c4a52d4485ec3ccb46662c2ccf9a92d4bdb744f4
Irene Kilgannon <irene.kilgannon@gmail.com>
2025-05-18 14:22:01 +0100

    correct column name in users table

--- db.sql
+++ db.sql
@@ -5,7 +5,7 @@ create table users (
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(320) NOT NULL,
-    password VARCHAR (255),
+    password_hash VARCHAR (255),
     PRIMARY KEY (userID)
 );
 
